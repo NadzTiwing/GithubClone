8c1ba1fef2d0d9483f221b96d1a74b51a54f08bc
Nadia Tiwing <tiwingnadia@gmail.com>
2024-01-24 19:03:21 +0800

    update subject class

--- classes/subjects.js
+++ classes/subjects.js
@@ -1,10 +1,12 @@
-class Subjects {
+class NewSubject {
   constructor (id, name, teacher) {
     this.id = id;
     this.name = name;
     this.teacher = teacher;
   }
-  
+}
+
+class Subjects {
   getAll() {
     const teachers = window.localStorage.getItem("teachers");
     return JSON.parse(teachers) || [];
